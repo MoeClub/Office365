4b37479e911b42a1315e2f4f315109f366794dc2
MoeClub <32381827+MoeClub@users.noreply.github.com>
2019-12-29 01:45:44 +0800

    Add files via upload

---
+++ Office365.ps1
@@ -0,0 +1,140 @@
+﻿[CmdletBinding()]
+Param([String]$u, [String]$p, [String]$l, [Switch]$cn)
+<#
+  # Details Office365 SKU by Powershell 
+  #                  Author: MoeClub.org
+  
+  Name       : SKU
+  -------------------------------------
+  E3_MSDN    : DEVELOPERPACK
+  A1_Student : STANDARDWOFFPACK_STUDENT
+  A1_Faculty : STANDARDWOFFPACK_FACULTY
+  A1P_Student :STANDARDWOFFPACK_IW_STUDENT
+  A1P_Faculty :STANDARDWOFFPACK_IW_FACULTY
+
+#>
+# Allow script: "Set-ExecutionPolicy -ExecutionPolicy Bypass -Force"
+
+$CommandList = (Get-Command -All)
+If (-Not ("Connect-MsolService" -in $CommandList.Name)) { Write-Host "`nInstall..."; Install-Module -Scope CurrentUser -Name MSOnline -Force }
+If ([String]::IsNullOrEmpty($($u).Trim())) { 
+  Do { $User = (Read-Host "Microsoft Office365 UserName") } While ([String]::IsNullOrEmpty($($User).Trim())) 
+} Else {
+  $User = $u
+}
+If ([String]::IsNullOrEmpty($($p).Trim())) { 
+  Do { $Passwd = (Read-Host "Microsoft Office365 Password") } While ([String]::IsNullOrEmpty($($Passwd).Trim()))
+} Else {
+  $Passwd = $p
+}
+$SecureString = ConvertTo-SecureString -AsPlainText "${Passwd}" -Force
+$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${User},${SecureString}
+Write-Host "`nLogin: ${User}"
+if ($cn) {
+  Connect-MsolService -Credential $MySecureCreds -AzureEnvironment AzureChinaCloud 2>&1>$null
+} else {
+  Connect-MsolService -Credential $MySecureCreds 2>&1>$null
+}
+If (-Not $?) { Write-Host "Error: Authentication."; Exit 1; }
+$UserRole = (Get-MsolUserRole -UserPrincipalName "${User}").Name
+$UserDetails = (Get-MsolUser -UserPrincipalName "${User}")
+$UserSKU_Full = (Get-MsolAccountSku)
+$UserORG = ($UserSKU_Full.AccountSkuId -Split ":")[0]
+If ([String]::IsNullOrEmpty($UserORG)) { Write-Host "Error: Office365 Name." }
+$UserSKU = $UserSKU_Full.AccountSkuId
+If ([String]::IsNullOrEmpty($UserRole)) { $UserRole = "User" }
+Write-Host "User Role: ${UserRole}"
+$AdminGuid = (Get-MsolRole -RoleName "Company Administrator").ObjectId.Guid
+If (-not ([String]::IsNullOrEmpty($($AdminGuid).Trim()))) {
+  Function QueryAdmin($AdminUser){
+    If (-not ([String]::IsNullOrEmpty($AdminUser))) {
+      $AdminBlock = (Get-MsolUser -UserPrincipalName "${AdminUser}").BlockCredential
+      If ("$AdminBlock" -eq "False") { $AdminStatus = "[Enable]" } Else { $AdminStatus = "[Disable]" }
+      Return -Join("$AdminStatus", "$AdminUser", "; ")
+    }
+  }
+  $AdminList = (Get-MsolRoleMember -RoleObjectId $AdminGuid).EmailAddress
+  If (-not ([String]::IsNullOrEmpty($($AdminList)))) {
+    $AdminRole = ""
+    If ($AdminList -is [String]) {
+      $AdminRole = (QueryAdmin $AdminList)
+    } ElseIf ($AdminList -is [Array]) {
+      For($i=0; $i -lt $AdminList.Count;$i++){
+        $AdminRole += (QueryAdmin $AdminList[$i])
+      }
+    }
+  } Else {
+    $AdminRole = "[Null]"
+  }
+  If (-not ([String]::IsNullOrEmpty($($AdminRole)))) {
+    Write-Host "Admin User: ${AdminRole}"
+  }
+}
+$UserDomain = (Get-MsolDomain)
+$UserDomain_Item = ""
+For($i=0; $i -lt $UserDomain.Count; $i++) {
+  $Domain_Name = $UserDomain[$i].Name
+  $Domain_Default = $UserDomain[$i].IsDefault
+  If ($Domain_Default -eq $True){
+    $Domain_Status = "Default"
+  } Else {
+    $Domain_Status = $UserDomain[$i].Status
+  }
+  $UserDomain_Item += -Join("[", "${Domain_Status}", "]", "${Domain_Name}", "; ")
+}
+Write-Host "Office365 Domain: ${UserDomain_Item}"
+If (-not ([String]::IsNullOrEmpty($($l).Trim()))) {
+  If ("$l" -eq "_"){ Exit 0 }
+  If (-Join("$UserORG", ":", "$l") -in $UserSKU) {
+    $QueryItem = $l
+  } Else {
+    $l = ""
+  }
+}
+If ([String]::IsNullOrEmpty($($l).Trim())) {
+  $QueryItem = ""
+  If ($UserSKU -is [Array]){
+    If ($UserSKU.Count -gt 0) {
+      $SKUITEM = ""
+      For($i=0; $i -lt $UserSKU.Count; $i++){ $SKUITEM += -Join(($UserSKU[$i] -Split ":")[1], "; ") }
+      Write-Host "Office365 SKU: ${SKUITEM}"
+      Do { $QueryItem = (Read-Host "Query Office365 SKU") } While (-not (-Join("$UserORG", ":", "$QueryItem") -in $UserSKU))
+      Write-Host "`n"
+    } Else {
+      Write-Host "Error: Not found subscription."; Exit 1
+    }
+  } Else {
+    $QueryItem = ($UserSKU -Split ":")[1]
+  }
+}
+$skuDetails = ($UserSKU_Full | where {$_.AccountSkuId -eq -Join("$UserORG", ":", "$QueryItem")})
+$SubscriptionId = ($skuDetails.SubscriptionIds.Guid)
+If ([String]::IsNullOrEmpty($SubscriptionId)) { Write-Host "Error: Subscription."; Exit 1 }
+Function QueryStatus($SubscriptionId){
+  $SubscriptionDetails = (Get-MsolSubscription -SubscriptionId $SubscriptionId)
+  $SubscriptionDate = ($SubscriptionDetails.DateCreated -Split " ")[0]
+  $SubscriptionStatus = "$($SubscriptionDetails.Status)"
+  Write-Host "TotalUnits: $($SubscriptionDetails.TotalLicenses)"
+  If ("$($SubscriptionDetails.IsTrial)" -eq "False") {
+    Write-Host "SubscriptionStatus: ${SubscriptionStatus}"
+  } Else {
+    Write-Host "SubscriptionStatus: [Trial] ${SubscriptionStatus}"
+  }
+  Write-Host "SubscriptionDate: ${SubscriptionDate}"
+  Write-Host "SubscriptionId: ${SubscriptionId}"
+  If ("$SubscriptionStatus" -eq "Enabled") { Return 0 } Else { Return 1 }
+}
+$SkuPartNumber = "$($skuDetails.SkuPartNumber)"
+Write-Host "SkuName: ${SkuPartNumber}"
+Write-Host "ActiveUnits: $($skuDetails.ActiveUnits)"
+Write-Host "ConsumedUnits: $($skuDetails.ConsumedUnits)"
+If ($SubscriptionId -Is [String]) {
+  $ReturnCode = (QueryStatus $SubscriptionId)
+} ElseIf ($SubscriptionId -Is [Array]) {
+  For($i=0;$i -lt $SubscriptionId.Count;$i++){
+    Write-Host "Item: ${SkuPartNumber}[$($i+1)]"
+    $ReturnList += (QueryStatus $SubscriptionId[$i])
+  }
+  If (1 -in $ReturnList){ $ReturnCode = 1 } Else { $ReturnCode = 0 }
+}
+If ("$ReturnCode" -eq "0") { Exit 0 } Else { Exit 1 }
---
+++ Office365_OneDrive.ps1
@@ -0,0 +1,69 @@
+﻿[CmdletBinding()]
+Param([String]$u, [String]$p, [String]$q, [Switch]$cn)
+<#
+  # Set Storage Quota for OneDrive for Busniess by Powershell 
+  #                                  Author: MoeClub.org
+#>
+
+$CommandList = (Get-Command -All)
+If (-Not ("Connect-MsolService" -in $CommandList.Name)) { Write-Host "`nInstall MSOnline..."; Install-Module -Scope CurrentUser -Name MSOnline -Force }
+If (-Not ("Connect-SPOService" -in $CommandList.Name)) { Write-Host "`nInstall Microsoft.Online.SharePoint.PowerShell..."; Install-Module -Scope CurrentUser -Name Microsoft.Online.SharePoint.PowerShell -Force }
+If ([String]::IsNullOrEmpty($($u).Trim())) { 
+  Do { $User = (Read-Host "Microsoft Office365 UserName") } While ([String]::IsNullOrEmpty($($User).Trim()))
+} Else {
+  $User = $u
+}
+If ([String]::IsNullOrEmpty($($p).Trim())) { 
+  Do { $Passwd = (Read-Host "Microsoft Office365 Password") } While ([String]::IsNullOrEmpty($($Passwd).Trim()))
+} Else {
+  $Passwd = $p
+}
+If ([String]::IsNullOrEmpty($($q).Trim())) { 
+    $SetQuotaInt = "{0:n1}" -f 5.0
+} else {
+    $SetQuotaInt = [convert]::ToDouble($q)
+    If (-Not $?) { Write-Host "Error: Invalid Quota."; Exit 1; }
+    $SetQuotaInt = "{0:n1}" -f $SetQuotaInt
+    if (${SetQuotaInt} -le ("{0:n1}" -f 0)) { Write-Host "Error: Quota must be greater than 0."; Exit 1; }
+}
+$SecureString = ConvertTo-SecureString -AsPlainText "${Passwd}" -Force
+$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${User},${SecureString}
+Write-Host "`nLogin: ${User}"
+if ($cn) {
+  Connect-MsolService -Credential $MySecureCreds -AzureEnvironment AzureChinaCloud 2>&1>$null
+} else {
+  Connect-MsolService -Credential $MySecureCreds 2>&1>$null
+}
+If (-Not $?) { Write-Host "Error: Login."; Exit 1; }
+$UserORG = ((Get-MsolDomain |where { $_.IsInitial -eq $true }).Name -Split("\.", 2))[0]
+if ($cn) { $EnvUrl = "sharepoint.cn" } else { $EnvUrl = "sharepoint.com" }
+Connect-SPOService -Url ('https://{0}-admin.{1}' -f ($UserORG, $EnvUrl)) -Credential $MySecureCreds
+If (-Not $?) { Write-Host "Error: Authentication."; Exit 1; }
+$SetQuotaFlag = $false
+$SetQuotaNum = 0
+$StorageQuota = [Math]::floor(5.1 * 1024 * 1024)
+Do {
+$SetQuotaNum += 1
+Write-Host "Setting: Pre-allocation Storage Quota to ${SetQuotaInt}TB"
+Set-SPOTenant -OneDriveStorageQuota $StorageQuota
+$CurrentQuota = "{0:n1}" -f ((Get-SPOTenant).OneDriveStorageQuota / (1024 * 1024))
+Write-Host "Current: Pre-allocation Storage Quota is ${CurrentQuota}TB"
+if ($SetQuotaInt -eq $CurrentQuota){ $SetQuotaFlag = $true; break }
+if ($SetQuotaNum > 7){Write-Host "Error: Current Storage Quota ${CurrentQuota}."; Exit 1;}
+} while (!($SetQuotaFlag))
+Write-Host "Reading: All user in [${UserORG}]"
+$UserMatch = ("-my.{0}/personal/" -f ($EnvUrl))
+$AllUrl = $(Get-SPOSite -IncludePersonalSite $true).Url | where { $_ -match $UserMatch }
+Function SetStorage($UserUrl) {
+  $CurrentUser = ([String]([String]$UserUrl -split "/")[-1] -split "_")[0]
+  Write-Host "Setting: Storage Quota to ${SetQuotaInt}TB for [${CurrentUser}]"
+  Set-SPOSite -StorageQuota $StorageQuota -Identity ${UserUrl}
+}
+$UrlType = ($AllUrl.GetType()).Name
+If ($UrlType -eq "Object[]") {
+  For ($i=0; $i -lt $AllUrl.Count; $i++) {
+    SetStorage $AllUrl[$i].ToString()
+  }
+} ElseIf ($UrlType -eq "String") {
+  SetStorage $AllUrl.ToString()
+}
